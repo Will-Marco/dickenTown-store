3c2c2a7ce62209331521e3b604253e83c2919e1c
Will-Marco <mr.marco0101@gmail.com>
2023-11-24 21:39:37 +0500

    add Hero and Product cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-export default function Home() {
+import Product from "@/components/Product";
+import { ProductType } from "./interfaces";
+import Hero from "@/components/Hero";
+
+const Home = async () => {
+  const products: ProductType[] = await fetch(
+    "https://fakestoreapi.com/products"
+  ).then((res) => res.json());
+
   return (
-    <div>HomePage</div>
-  )
-}
+    <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0 ">
+      <Hero />
+      <section className="flex flex-col space-y-12">
+        <h1 className="text-5xl font-bold text-center">SHOP DEALS</h1>
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/components/Product.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { FC } from "react";
+import { ProductType } from "@/app/interfaces";
+
+const Product: FC<{ product: ProductType }> = ({ product }) => {
+  return (
+    <div className="bg-gray-100 p-6 rounded-lg">
+      <img
+        className="h-40 rounded w-full object-cover object-center mb-6"
+        src="https://dummyimage.com/720x400"
+        alt="content"
+      />
+      <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+        {product.category}
+      </h3>
+      <div className="font-semibold flex items-center justify-between mt-4 mb-1">
+        <p className="w-44 truncate">{product.title}</p>
+        <p>${product.price}</p>
+      </div>
+      <p className="leading-relaxed text-base line-clamp-2">
+        {product.description}
+      </p>
+    </div>
+  );
+};
+
+export default Product;
