52e11de0d5788b72e5c36f08311284087d3b95e7
Will-Marco <mr.marco0101@gmail.com>
2023-11-25 11:25:20 +0500

    add image optimization

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { FC } from "react";
+import Link from "next/link";
 import { ProductType } from "@/app/interfaces";
+import CustomImage from "./Custom-Image";
 
 const Product: FC<{ product: ProductType }> = ({ product }) => {
   return (
-    <div className="bg-gray-100 p-6 rounded-lg">
-      <img
-        className="h-40 rounded w-full object-cover object-center mb-6"
-        src="https://dummyimage.com/720x400"
-        alt="content"
-      />
-      <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+    <Link href={`/product/${product.id}`} className="h-96 p-6 flex flex-col bg-white border rounded-lg hover:scale-105 transition-transform ease-out duration-200 ">
+      <div className="max-h-72 flex-1 relative">
+        <CustomImage product={product} fill/>
+      </div>
+      <h3 className="mt-5 tracking-widest text-indigo-500 text-xs font-medium title-font">
         {product.category}
       </h3>
       <div className="font-semibold flex items-center justify-between mt-4 mb-1">
@@ -21,7 +21,7 @@ const Product: FC<{ product: ProductType }> = ({ product }) => {
       <p className="leading-relaxed text-base line-clamp-2">
         {product.description}
       </p>
-    </div>
+    </Link>
   );
 };
 
