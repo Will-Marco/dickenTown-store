cefa711500c34bea28b8e847858e39c2c1e255a9
Will-Marco <mr.marco0101@gmail.com>
2023-11-29 19:27:31 +0500

    add increment and decrement handler

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -3,11 +3,52 @@
 import CustomImage from "@/components/Custom-Image";
 import { ProductType } from "../interfaces";
 import ReactStars from "react-stars";
+import { useState } from "react";
 
 const ShoppingCart = () => {
-  const products: ProductType[] =
-    JSON.parse(localStorage.getItem("carts") as string) || [];
-  console.log(products);
+  const [products, setProducts] = useState<ProductType[]>(
+    JSON.parse(localStorage.getItem("carts") as string) || []
+  );
+
+  const removeProduct = (id: number) => {
+    const updatedCart = products.filter((product) => product.id !== id);
+    localStorage.setItem("carts", JSON.stringify(updatedCart));
+    setProducts(updatedCart);
+  };
+
+  const incrementHandler = (id: number) => {
+    const updatedCart = products.map((product) => {
+      if (product.id === id) {
+        return {
+          ...product,
+          quantity: product.quantity + 1,
+        };
+      }
+      return product;
+    });
+    localStorage.setItem("carts", JSON.stringify(updatedCart));
+    setProducts(updatedCart);
+  };
+
+  const decrementHandler = (id: number) => {
+    const existProduct = products.find((product) => product.id === id);
+    if (existProduct?.quantity === 1) {
+      removeProduct(existProduct.id);
+    } else {
+      const updatedCart = products.map((product) => {
+        if (product.id === id) {
+          return {
+            ...product,
+            quantity: product.quantity - 1,
+          };
+        }
+
+        return product;
+      });
+      localStorage.setItem("carts", JSON.stringify(updatedCart));
+      setProducts(updatedCart);
+    }
+  };
 
   return (
     <div className="h-screen bg-gray-100 pt-20">
@@ -44,7 +85,10 @@ const ShoppingCart = () => {
                 </div>
                 <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                   <div className="ml-5 flex items-center border-gray-100">
-                    <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                    <span
+                      className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"
+                      onClick={() => decrementHandler(product.id)}
+                    >
                       {" "}
                       -{" "}
                     </span>
@@ -54,18 +98,36 @@ const ShoppingCart = () => {
                       value={product.quantity}
                       min="1"
                     />
-                    <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                    <span
+                      className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
+                      onClick={() => incrementHandler(product.id)}
+                    >
                       {" "}
                       +{" "}
                     </span>
                   </div>
-                  <div className="flex items-center space-x-4">
+                  <div className="flex items-baseline space-x-4">
                     <p className="text-sm">
                       {product.price.toLocaleString("en-US", {
                         style: "currency",
                         currency: "USD",
                       })}
                     </p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+                      onClick={() => removeProduct(product.id)}
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
                   </div>
                 </div>
               </div>
